a2c3a5d980befbbc1c2deb641e75c96b2e849f7c
5cf <1207574336@qq.com>
2023-04-07 21:34:46 +0800

    测试

--- oj/src/schooloj/链表排序/Solution.java
+++ oj/src/schooloj/链表排序/Solution.java
@@ -28,5 +28,5 @@ class ListNode {
     ListNode next;
     ListNode() {}
     ListNode(int val) { this.val = val; }
-    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next;  }
 }
